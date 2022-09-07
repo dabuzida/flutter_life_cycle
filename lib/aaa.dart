bc49cc0d90878217138b4e6bb6f2e7f31cc10bd6
dabuzida <dabuzida@gmail.com>
2022-09-07 17:58:15 +0900

    test lifecycle

--- lib/aaa.dart
+++ lib/aaa.dart
@@ -2,15 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+// createState() 실행 >>
+
+// ' _MyStatefulWidgetState의 멤버변수 생성>> _MyStatefulWidgetState의 부모 State() >> _MyStatefulWidgetState() 생성'
+
+// >>
+// createState()가 _MyStatefulWidgetState() 반환
+
+// >>
+
+// initState() >> didChangeDependencies() >> build(): build 멤버변수 >> build return된것 실행
+
 class AAA extends StatefulWidget {
   const AAA({Key? key}) : super(key: key);
 
   @override
   State<AAA> createState() {
+    print('createState()');
     _AAAState newObj = _AAAState();
-    print('>> ${newObj.name}');
-    newObj.name = 'flutter test';
-    print('1 >> ${newObj.name}');
+    // print('>> ${newObj.name}');
+    // newObj.name = 'flutter test';
+    // print('1 >> ${newObj.name}');
     return newObj;
   }
 }
@@ -20,17 +32,27 @@ class _AAAState extends State<AAA> {
 
   @override
   void initState() {
+    print('initState()');
     super.initState(); // 나의 코드 구현 전에 써야함? << 라이프사이클 별 확인필요
     name = 'flutter test2';
-    print('2 >> $name');
+    // print('2 >> $name');
   }
 
   @override
   void didChangeDependencies() {
+    print('didChangeDependencies()');
     // Provider를 여기서 호출하니 이 함수가 2번 호출된적이 있음
     name = 'flutter test3';
-    print('3 >> $name');
+    // print('3 >> $name');
     super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build()');
+    // print('4 >> $name');
+
+    return Center(child: Text(name!));
   }
 
   @override
@@ -53,12 +75,5 @@ class _AAAState extends State<AAA> {
     print('didUpdateWidget()');
 
     super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('4 >> $name');
-
-    return Center(child: Text(name!));
   }
 }
